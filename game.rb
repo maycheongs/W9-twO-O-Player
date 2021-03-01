bad521607afcaafcaa50f0cda483e468b3650fa9
May C <maycheongs@gmail.com>
2021-03-01 22:22:19 +0000

    random question generated

--- game.rb
+++ game.rb
@@ -1,12 +1,14 @@
 require './person'
 
 class Game
-  attr_accessor :turn
+  attr_accessor :turn, :question, :answer
 
   def initialize(player1, player2)
     @turn = 1
     @player1 = player1
     @player2 = player2
+    @question = []
+    @answer = ''
   end
 
   def next_turn
@@ -15,5 +17,16 @@ class Game
 
   def current_player
     @turn % 2 == 0 ? @player2 : @player1
+  end
+
+  def new_question()
+    num1 = rand(20) + 1
+    num2 = rand(20) + 1
+    @question = [num1, num2]
+    puts "What does #{num1} and #{num2} equal?"
+  end
+
+  def answer()
+    @answer = @question[0] + @question[1]
   end
 end
--- play.rb
+++ play.rb
@@ -5,26 +5,14 @@ player1 = Player.new('Player 1')
 player2 = Player.new('Player 2')
 game = Game.new(player1, player2)
 
-questions = [
-  {},
-  { question: 'What does 5 + 6 equal?', answer: 11 },
-  { question: 'What does 2 plus 6 equal', answer: 8 },
-  { question: '10 times 10 equals?', answer: 100 },
-  { question: '200 divided by 50 equals?', answer: 4 },
-  { question: '3 times 3 equals?', answer: 9 },
-  { question: '4 plus 4 equals?', answer: 8 },
-  { question: '2 minus 2 equals?', answer: 0 },
-  { question: '6 divided by 2 equals?', answer: 3 },
-]
-
 puts 'Welcome to tWO-O-Player'
 puts 'You each have 3 lives, and the player who loses all 3 lives loses. The remaining player wins!'
 
 while !player1.dead? && !player2.dead?
   puts '-------------------'
-  puts "#{game.current_player.name}: #{questions[game.turn][:question]}"
+  puts "#{game.current_player.name}: #{game.new_question}"
   answer = gets.chomp.to_i
-  if answer == questions[game.turn][:answer]
+  if answer == game.answer
     puts 'YES! You are correct'
     game.next_turn
   else
